2b0c0d072c638e2fd2b29cda407ec91ab1d41fa4
Alexandre Saudate <alesaudate@gmail.com>
2015-11-16 13:21:58 -0200

    Update sqldeveloper.bash

--- sqldeveloper.bash
+++ sqldeveloper.bash
@@ -1,3 +1,3 @@
 xhost +
 docker pull guywithnose/sqldeveloper
-docker run -i -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix guywithnose/sqldeveloper
+docker run -i -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --link db guywithnose/sqldeveloper
