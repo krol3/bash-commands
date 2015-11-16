83807d9cbb1f5907b7eb51ad7e1426f2adc72902
Alexandre Saudate <alesaudate@gmail.com>
2015-11-16 10:39:05 -0200

    Update ember.bash

--- ember.bash
+++ ember.bash
@@ -1,2 +1,2 @@
 docker pull visualjeff/ember-cli
-docker run -i -v `pwd`:/root visualjeff/ember-cli ${command} ${args[@]}
+echo 'docker run -i -v `pwd`:/root visualjeff/ember-cli ${command} ${args[@]}'
