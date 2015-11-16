89bc0dc0a9962295f0e84546dfd3629eb0f0966f
Alexandre Saudate <alesaudate@gmail.com>
2015-11-16 16:48:52 -0200

    Update dominio12c.bash

--- dominio12c.bash
+++ dominio12c.bash
@@ -1,2 +1,1 @@
-docker run -d --name=db -p 1521:1521 -p 8080:8080 alexeiled/docker-oracle-xe-11g
 mkdir -p ~/weblogic12c && chmod 777 ~/weblogic12c && docker run -i -v ~/weblogic12c:/opt/oracle/wlsdomains -e USER=$USER --link db sciensa2/soasuite12c-domain && sudo chown -R $USER:$USER ~/weblogic12c
